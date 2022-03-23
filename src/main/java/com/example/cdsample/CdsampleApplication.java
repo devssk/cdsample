fecdb1d707a1a6717873b2ba58d2f840745b2d32
devssk <sksong419@naver.com>
2022-03-23 18:02:15 +0900

    Initial commit

---
+++ src/main/java/com/example/cdsample/CdsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdsampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cdsample/CdsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdsampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
